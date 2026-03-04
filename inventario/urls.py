36b965d6293fc11fd4a6587f91dfe0dcafcc4841
camilouan <cparra78@uan.edu.co>
2026-03-04 16:01:30 -0500

    modificacion de vista entre visitante y usuario o cliente

--- inventario/urls.py
+++ inventario/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('apartar/<int:producto_id>/', views.apartar_producto, name='apartar'),
-    path('registro/', views.registro, name='registro'),
-    path('login/', views.login_view, name='login'),
-path('logout/', views.logout_view, name='logout'),
 
+    path('', views.landing, name='landing'),
+
+    path('productos/', views.productos, name='productos'),
+
+    path('apartar/<int:producto_id>/', views.apartar_producto, name='apartar'),
+
+    path('registro/', views.registro, name='registro'),
+
+    path('login/', views.login_view, name='login'),
+
+    path('logout/', views.logout_view, name='logout'),
 
 ]
