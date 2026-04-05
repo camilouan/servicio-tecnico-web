68e18825cc236a94eb81ec370d7c4cfcc7ea880d
camilouan <cparra78@uan.edu.co>
2026-04-05 16:34:54 -0500

    corrigiendo pie de pagina y diseño

--- servicio_tecnico/settings.py
+++ servicio_tecnico/settings.py
@@ -140,10 +140,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_USE_FINDERS = True
 
