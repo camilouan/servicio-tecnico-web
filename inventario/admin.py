0f5ed5d8c7fdf64bd1ed75906b16f96bce5a0920
camilouan <cparra78@uan.edu.co>
2026-04-05 17:07:27 -0500

    mejorando UI

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Usuario, Producto, Apartado, Categoria
+from .models import Usuario, Producto, Apartado, Categoria, HeroBanner
 
 
 admin.site.register(Producto)
 admin.site.register(Apartado)
 admin.site.register(Categoria)
+admin.site.register(HeroBanner)
 
 
 @admin.register(Usuario)
