71d8910e5cf361873239d05a15a2e26d8919fd73
camilouan <cparra78@uan.edu.co>
2026-05-26 01:07:20 -0500

    fix render v1

--- servicio_tecnico/settings.py
+++ servicio_tecnico/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', '.onrenderer.com', 'localhost', '127.0.0.1']
 
 
 # -------------------------
